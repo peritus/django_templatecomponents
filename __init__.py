86f29eca0dbd3e8d90583a226844135cfb7d4bca
Filip <filip@j03.de>
2008-06-18 12:45:03 +0200

    simplified template tag registration (thx, bartTC)

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.template import add_to_builtins
 
-add_to_builtins(settings.SETTINGS_MODULE.split('.')[0] + '.django_templatecomponents.templatetags.templatecomponenttags')
+add_to_builtins('django_templatecomponents.templatetags.templatecomponenttags')

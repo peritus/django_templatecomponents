b63ace5718b02f37ea5c0ad1ef90930ae02ae690
Filip Noetzel <filip@j03.de>
2008-09-14 16:13:38 +0200

    add view again, middleware-magic was a pony-idea.

---
+++ views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse, Http404
+from django.views.static import serve
+from mimetypes import guess_type
+
+import templatecomponents
+
+def generate_templatecomponents(request, path):
+    group, extension = path.rsplit(".", 1)
+
+    collected = str(templatecomponents.all().without_inline().filter(extension).group(group))
+
+    if not collected:
+        av = ", ".join(["%s.%s" % x for x in templatecomponents.all().available()])
+        raise Http404("templatecomponent '%s.%s' not found. Available choices: %s" % (group, extension, av,))
+
+    return HttpResponse(collected, content_type=guess_type(path)[0])
